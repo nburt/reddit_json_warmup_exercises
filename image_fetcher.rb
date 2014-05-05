cc0ed426314143898b06ac121fd0e1553a4c5ee3
Nathanael Burt <nathanael.burt@gmail.com>
2014-05-05 09:25:39 -0600

    completed image fetcher warmup assignment

--- image_fetcher.rb
+++ image_fetcher.rb
@@ -7,15 +7,28 @@ class ImageFetcher
   end
 
   def count_children
+    @data["data"]["children"].count
   end
 
   def first_child
+    @data["data"]["children"].first["data"]
   end
 
   def all_images
+    @data["data"]["children"].map do |child|
+      child["data"]["url"]
+    end
   end
 
   # fetch only the images that begin with `http://i.imgur.com`
   def only_imgur_images
+    imgur_images = []
+    @data["data"]["children"].each do |child|
+      if child["data"]["url"].match /http:\/\/i.i/
+        imgur_images << child["data"]["url"]
+        @data.delete_if {|k, v| v.nil?}
+      end
+    end
+    imgur_images
   end
 end
